cc179f2deb57f0f24903f284c902a367caa30178
464lalosegura <jimenezseguraeduardo@gmail.com>
2024-10-31 15:48:04 -0600

    agrgegue la ruta del puerto en el flask

--- app.py
+++ app.py
@@ -2,7 +2,12 @@ from flask import Flask, render_template, request, redirect, url_for, session, f
 from db_connection import create_connection, close_connection
 
 app = Flask(__name__)
-app.secret_key = 'supersecretkey'  # Clave secreta para manejar las sesiones
+app.secret_key = 'supersecretkey'
+
+# Ruta para la página de inicio, redirige al inicio de sesión
+@app.route("/")
+def inicio():
+    return redirect(url_for("login"))
 
 # Ruta para la página de inicio de sesión
 @app.route("/login", methods=["GET", "POST"])
@@ -11,30 +16,30 @@ def login():
         usuario = request.form["usuario"]
         contrasena = request.form["contrasena"]
         
-        # Conectarse a la base de datos para verificar las credenciales
+        # Conectarse a la base de datos para verificar credenciales
         conn = create_connection()
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM usuario WHERE nombre = %s AND contraseña = %s", (usuario, contrasena))
         user = cursor.fetchone()
         
-        # Si las credenciales son correctas, redirigir al panel de control
+        # Si las credenciales son correctas, redirigir al panel de usuario
         if user:
             session["usuario"] = usuario
             session["usuario_id"] = user[0]  # Almacena el ID del usuario en la sesión
-            return redirect(url_for("panel_control"))
+            return redirect(url_for("panel_usuario"))
         else:
-            # Si las credenciales no son válidas, mostrar un mensaje de error
+            # Si las credenciales no son válidas, mostrar mensaje de error
             error = "Usuario o contraseña incorrectos"
             return render_template("iniciar_sesion.html", error=error)
     
     return render_template("iniciar_sesion.html")
 
-# Ruta para el panel de control después de iniciar sesión
-@app.route("/panel_control")
-def panel_control():
+# Ruta para el panel de usuario
+@app.route("/panel_usuario")
+def panel_usuario():
     if "usuario" in session:
-        # Aquí puedes mostrar el CRUD para pedidos, reclamos, etc.
-        return render_template("panel_control.html", usuario=session["usuario"])
+        # Muestra el CRUD para pedidos, reclamos, etc.
+        return render_template("panel_de_usuario.html", usuario=session["usuario"])
     else:
         return redirect(url_for("login"))
 
@@ -71,11 +76,11 @@ def enviar_reclamo():
     close_connection(conn)
     
     flash("Tu reclamo fue enviado con éxito.")
-    return redirect(url_for("panel_control"))
+    return redirect(url_for("panel_usuario"))
 
 @app.route('/quejas')
 def quejas():
     return render_template('quejas.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="127.0.0.1", port=5000)
